772a83be788647a1737357aacd5c150661a96e0c
YIFANG CHEN <yifangc@google.com>
2025-04-09 19:44:02 +0000

    Add more evaluation conditions.

--- deepscaler/scripts/eval/eval_enforceans.sh
+++ deepscaler/scripts/eval/eval_enforceans.sh
@@ -15,25 +15,12 @@ echo "Datasets: ${DATATYPES[@]}"
 echo "Output Directory: ${OUTPUT_DIR}"
 
 # Loop through all datatypes
-    python3 -m verl.trainer.main_generation \
-        trainer.nnodes=1 \
-        trainer.n_gpus_per_node=8 \
-        data.train_ratio=0.025 \
-        +data.train_ratio_seed=42 \
-        data.path=$HOME/data/deepscaler/train.parquet \
-        data.output_path="/mnt/disk3/verl/eval/${PROJECT_NAME}/${EXPERIMENT_NAME}/gen.parquet" \
-        data.n_samples=8 \
-        data.batch_size=1024 \
-        model.path=${MODEL_PATH} \
-        rollout.temperature=0.6 \
-        rollout.response_length=2048 \
-        rollout.top_k=-1 \
-        rollout.top_p=0.95 \
-        rollout.gpu_memory_utilization=0.9 \
-        rollout.tensor_model_parallel_size=1 \
-        rollout.force_append_answers=True
 
-    python3 -m verl.trainer.main_eval \
-        data.path="/mnt/disk3/verl/eval/${PROJECT_NAME}/${EXPERIMENT_NAME}/gen.parquet" \
-        data.response_key="response" \
-        data.edit_response_key="edit_response" 
+python3 -m verl.trainer.main_eval \
+    data.path="/mnt/disk3/verl/eval/${PROJECT_NAME}/${EXPERIMENT_NAME}/gen.parquet" \
+    data.response_key="response" \
+    data.edit_response_key="edit_response" \
+    data.n_pass=4 \
+    data.max_response_length=2048 \
+    data.difficulty_key="difficulty" \
+    output_dir="/mnt/disk3/verl/eval/${PROJECT_NAME}/${EXPERIMENT_NAME}/eval_all"
