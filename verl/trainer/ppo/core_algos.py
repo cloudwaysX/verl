46c69989d060bc349847325e4e9fcb5e2e88321a
cloudwaysX <yifangc@cs.washington.edu>
2025-05-29 06:05:15 +0000

    Fix track length var.

--- verl/trainer/ppo/core_algos.py
+++ verl/trainer/ppo/core_algos.py
@@ -133,6 +133,7 @@ def compute_grpo_outcome_advantage(token_level_rewards: torch.Tensor,
     rewards_std = torch.zeros_like(scores)
     rewards_mean_per_sample = torch.zeros_like(scores)
     edit2correct_per_sample = torch.zeros_like(scores)
+    lenstd = torch.zeros_like(scores)
 
     id2score = defaultdict(list)
     id2len = defaultdict(list)
@@ -154,11 +155,13 @@ def compute_grpo_outcome_advantage(token_level_rewards: torch.Tensor,
                 id2lenstd[idx] = torch.tensor(0.0)
             elif len(id2score[idx]) > 1:
                 id2mean[idx] = torch.mean(torch.tensor(id2score[idx]))
-                id2std[idx] = torch.std(torch.tensor([id2score[idx]]))
+                id2std[idx] = torch.std(torch.tensor(id2score[idx]))
                 # If we are in binary setting, we only count the number of edits when the score is > 0 and < 1
                 # TODO(yifangc): what if in the future we have more than binary setting?
                 id2count_edit2correct[idx] = float(sum(1 for score in id2score[idx] if score > 0 and score < 1))
-                id2lenstd[idx] = torch.std(torch.tensor(id2len[idx]))
+                # print("DEBUG id2len", id2len[idx])
+                # print("DEBUG id2score", id2score[idx])
+                id2lenstd[idx] = torch.std(torch.tensor(id2len[idx], dtype=torch.float32))
             else:
                 raise ValueError(f"no score in prompt index: {idx}")
         for i in range(bsz):
@@ -166,7 +169,7 @@ def compute_grpo_outcome_advantage(token_level_rewards: torch.Tensor,
             rewards_std[i] = id2std[index[i]]
             rewards_mean_per_sample[i] = id2mean[index[i]]
             edit2correct_per_sample[i] = id2count_edit2correct[index[i]]
-            lenstd = id2lenstd[index[i]]
+            lenstd[i] = id2lenstd[index[i]]
         scores = scores.unsqueeze(-1).tile([1, response_length]) * eos_mask
 
     if return_std:
